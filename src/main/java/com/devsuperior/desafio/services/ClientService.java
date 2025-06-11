f250587deaa7068f9867098b19ba3ad6b21dc131
Jean Victor <jeanvictor.fa@gmail.com>
2025-06-11 15:46:17 -0300

    GET Client By ID

---
+++ src/main/java/com/devsuperior/desafio/services/ClientService.java
@@ -0,0 +1,23 @@
+package com.devsuperior.desafio.services;
+
+import com.devsuperior.desafio.dtos.ClientDTO;
+import com.devsuperior.desafio.entities.Client;
+import com.devsuperior.desafio.repositories.ClientRepository;
+import com.devsuperior.desafio.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ClientService {
+
+    @Autowired
+    private ClientRepository repository;
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id){
+        Client entity = repository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Cliente não encontrado com o ID:" + id));
+        return new ClientDTO(entity);
+    }
+}
